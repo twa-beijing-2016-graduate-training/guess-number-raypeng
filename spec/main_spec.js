f5dfc52c6be96bec1df6100f754295abf48ba7a8
Rui Peng <pengrui1993@gmail.com>
2016-02-29 00:20:18 +0800

    finish Q2

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -14,7 +14,6 @@ describe("Guessing Game", function() {
   var engine = GuessingGame();
 
   describe(".compare_number", function() {
-
     it("should give correct result in form of xAxB", function() {
       expect("0A0B").to.equal(engine.compare_number("1234", "5678"));
       expect("0A1B").to.equal(engine.compare_number("1234", "5671"));
@@ -26,7 +25,20 @@ describe("Guessing Game", function() {
       expect("1A3B").to.equal(engine.compare_number("1234", "1111"));
       expect("2A2B").to.equal(engine.compare_number("1234", "1243"));
     });
+  });
 
+  describe(".generate_random_number", function() {
+    it("should give random number comprised of 4 different digits", function() {
+      var check = function(num_string) {
+        var nums = num_string.split("");
+        var sorted = nums.sort();
+        return sorted[0] !== sorted[1] && sorted[1] !== sorted[2] && sorted[2] !== sorted[3];
+      }
+      for (var i = 0; i < 100; i++) {
+        var random_number = engine.generate_random_number();
+        expect(true).to.equal(check(random_number));
+      }
+    });
   });
 
 });
